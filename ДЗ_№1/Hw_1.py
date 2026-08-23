c5f69e163a5c485dfc3a63f21a3884c9ae1b8f72
Derek <ushakov_gd@mail.ru>
2026-08-23 04:32:12 +0300

    Решение 2 задачи

--- ДЗ_№1/Hw_1.py
+++ ДЗ_№1/Hw_1.py
@@ -1,3 +1,4 @@
+import math as m
 # Задание 1. Сумма цифр
 # Напишите фунĸцию digit_sum(number) .
 # Параметр number — целое число. Фунĸция должна вернуть сумму цифр этого числа.
@@ -21,3 +22,26 @@ def digit_sum(number: int) -> int:
         number //= 10
 
     return sum
+
+# Задание 2. Простое число
+# Напишите фунĸцию is_prime(number).
+# Параметр number — целое число. Фунĸция должна вернуть True, если число простое,
+# и False в противном случае. Простым считается целое число больше 1, ĸоторое
+# делится без остатĸа тольĸо на 1 и само себя. Все числа меньше 2 считаются
+# непростыми.
+
+# Решение:
+def is_prime(number: int) -> bool: # Ну его к лешему, перелопатить три! (мать его) теоремы, я изучаю Python а не разделы вышки.
+
+    if not isinstance(number, int):
+        raise TypeError("Не верный тип данных")
+
+    if number < 2:
+        return False
+
+    n1 = int(m.sqrt(number))
+    for i in range(2, n1 + 1):
+        if number % i == 0:
+            return False
+
+    return True
--- ДЗ_№1/test_Hw1.py
+++ ДЗ_№1/test_Hw1.py
@@ -1,7 +1,8 @@
 from hypothesis import given
 import hypothesis.strategies as st
-from Hw_1 import digit_sum
+from Hw_1 import digit_sum, is_prime
 
+# 1.
 @given(st.integers())
 def test_digit_sum_properties(number):
 
@@ -10,3 +11,19 @@ def test_digit_sum_properties(number):
 
     if 0 <= number <= 9:
         assert result == number
+
+# 2.
+@given(st.integers())
+def test_is_prime_properties(number):
+
+    if number < 2:
+        assert is_prime(number) is False
+        return
+
+    result = is_prime(number)
+
+    if number > 2 and number % 2 == 0:
+        assert result is False
+
+    if number in [2, 3, 5, 7, 11]:
+        assert result is True
